d97eed2d29f2a6119aa730713a1f926b160f14f1
Francisco <fnaveira11@gmail.com>
2021-03-25 23:22:00 -0400

    commit tree e2083c840b4798cb1b8c128d654c2fd9240b96d6

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
-console.log('%c HI', 'color: firebrick')
+const initialize = () => {
+    const dogPic = document.getElementById('dog-image-container')
+    
+    //Challenge 1: (Below)
+    fetch(`https://dog.ceo/api/breeds/image/random/4`)
+    .then(response => response.json())
+    .then(imgArry => {
+        imgArry.message.forEach(element => {
+            dogPic.innerHTML += `<img src="${element}" alt="dog pic">`
+        });
+    })
+
+    //Challenge 2:
+    function renderDogs(dogData) {
+       
+       for (const breedname of dogData){ 
+           const li= document.createElement('li')
+           li.innerText = breedname
+           const dogBreed = document.querySelector('ul#dog-breeds')
+            dogBreed.appendChild(li)
+            //Challenge 3: 
+            li.addEventListener('click', () => li.style.color= 'purple' )
+           }
+       }
+    fetch(`https://dog.ceo/api/breeds/list/all`)
+    .then(responseBreeds => responseBreeds.json())
+    .then(json => renderDogs(Object.keys(json.message)))
+    
+    //Challenge 4: 
+    const filterTest = (event) => {
+            const resultA =  dogs.filter(arry => arry[0] === event.target.value)
+            const dogBreed = document.querySelector('ul#dog-breeds')
+            dogBreed.querySelectorAll('*').forEach(n => n.remove());
+            renderDogs(resultA)
+    }
+    document.getElementById('breed-dropdown').addEventListener('change', filterTest)
+}
+
+document.addEventListener('DOMContentLoaded', initialize)
+
+
+
+
+
+
